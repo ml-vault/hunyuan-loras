7b24a737c38ee7a0a2d01d96b152e3c51cee1b32
Yoonsoo Kim <laptise@live.jp>
2025-03-08 01:08:07 +0900

    pass def

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,6 @@ FROM runpod/pytorch:2.1.1-py3.10-cuda12.1.1-devel-ubuntu22.04
 
 WORKDIR /opt
 
-RUN pip install runpod
-
 # pytorch to 2.5.1
 RUN pip install torch==2.5.1
 
@@ -19,12 +17,6 @@ RUN git clone --recurse-submodules https://github.com/tdrussell/diffusion-pipe.g
 # Install requirements
 RUN pip install -r /opt/diffusion-pipe/requirements.txt
 
-# Install Jupyter
-RUN python3 -m pip install jupyter notebook
+COPY start.sh /opt/start.sh
 
-COPY run.sh /opt/run.sh
-
-COPY notes /opt/notes
-
-CMD ["sh", "/opt/run.sh"]
-
+CMD ["sh", "/opt/start.sh"]
--- start.sh
+++ start.sh
@@ -1,4 +1,17 @@
-#! /bin/bash
+#!/bin/bash
+
+echo "pod started"
+
+if [[ $PUBLIC_KEY ]]
+then
+    mkdir -p ~/.ssh
+    chmod 700 ~/.ssh
+    cd ~/.ssh
+    echo $PUBLIC_KEY >> authorized_keys
+    chmod 700 -R ~/.ssh
+    cd /
+    service ssh start
+fi
 
 VOLUME_PATH=/workspace
 
@@ -21,7 +34,11 @@ fi
 sh $VOLUME_PATH/venv/bin/activate
 echo "Virtual environment activated"
 
-# Run Jupyter in background
-echo "Jupyter notebook started"
 
-jupyter notebook --ip=0.0.0.0 --port=8888 --no-browser --allow-root --notebook-dir=$VOLUME_PATH
+if [[ $JUPYTER_PASSWORD ]]
+then
+    cd /
+    jupyter lab --allow-root --no-browser --port=8888 --ip=* --ServerApp.terminado_settings='{"shell_command":["/bin/bash"]}' --ServerApp.token=$JUPYTER_PASSWORD --ServerApp.allow_origin=* --ServerApp.preferred_dir=/workspace
+else
+    sleep infinity
+fi
